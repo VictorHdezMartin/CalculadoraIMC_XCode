a50c83862c791a4cff1affe7b76bc2d0784ddf94
bermonch <doblado007@gmail.com>
2024-12-11 16:20:39 +0100

    Initial Commit

---
+++ Calculadora IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora IMC
+//
+//  Created by Tardes on 11/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculadora IMCTests/Calculadora_IMCTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora_IMCTests.swift
+//  Calculadora IMCTests
+//
+//  Created by Tardes on 11/12/24.
+//
+
+import Testing
+@testable import Calculadora_IMC
+
+struct Calculadora_IMCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
